b112501c5b8e44a4ee870cc8ec22ad234033b392
Maran9 <87171972+Maran9@users.noreply.github.com>
2021-09-07 17:59:37 +0530

    Add files via upload

---
+++ Chapter07/src/main/java/com/air/quality/scanner/model/AqiCountryResponse.java
@@ -0,0 +1,12 @@
+package com.air.quality.scanner.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AqiCountryResponse {
+    public List<Location> results;
+}
