7c623d9476842692e5354cc17d46ddb4ba468abc
Maran9 <87171972+Maran9@users.noreply.github.com>
2021-09-07 17:37:14 +0530

    Delete src directory

--- src/main/java/com/packt/chapter3/indianstates/State.java
+++
@@ -1,5 +0,0 @@
-package com.packt.chapter3.indianstates;
-
-
-public record State(String name, String capital) {
-}
--- src/main/java/com/packt/chapter3/indianstates/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.packt.chapter3.indianstates;
-
-import java.util.List;
-
-public interface StateRepository {
-    List<State> findAll();
-
-    String findByName(String name);
-}
